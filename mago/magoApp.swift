2ada1a8597e60316521a010426b7a8fdb157b59e
A14519 <a14519@A14519ui-MacBookPro.local>
2024-08-17 23:32:08 +0900

    Initial commit

---
+++ mago/magoApp.swift
@@ -0,0 +1,17 @@
+//
+//  magoApp.swift
+//  mago
+//
+//  Created by A14519 on 8/17/24.
+//
+
+import SwiftUI
+
+@main
+struct magoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ magoUITests/magoUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  magoUITestsLaunchTests.swift
+//  magoUITests
+//
+//  Created by A14519 on 8/17/24.
+//
+
+import XCTest
+
+final class magoUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
